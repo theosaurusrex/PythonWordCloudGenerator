361c2e82c9fc5a10f208225ef9c493443bff8bfc
theodore howard <theodorehoward@theodores-MacBook-Air.local>
2019-03-02 13:28:00 -0600

    Wordcloud works, manually take and add shop to photoshop

---
+++ wordCloudGen/wordcloudGen.py
@@ -0,0 +1,28 @@
+import wikipedia
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import os
+from PIL import Image
+import numpy as np
+currdir = os.path.dirname(__file__)
+
+def get_wiki(query):
+
+    title = wikipedia.search(query)[0]
+    page = wikipedia.page(title)
+    return page.content
+
+def create_wordcloud(text):
+    mask = np.array(Image.open(os.path.join(currdir, "thales-matt-bjj-cloud.png")))
+
+    stopwords = set(STOPWORDS)
+
+    wc = WordCloud(background_color="white",
+        mask=mask,
+        max_words=200,
+        stopwords=stopwords)
+        
+    wc.generate(text)
+
+    wc.to_file(os.path.join(currdir,"thales-matt-bjj-wc.png"))
+
+create_wordcloud(get_wiki("brazilian jiu jitsu"))
